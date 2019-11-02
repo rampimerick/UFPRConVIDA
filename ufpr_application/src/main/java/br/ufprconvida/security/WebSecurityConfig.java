2fae840ad06ab1c314b43d7f7a9102bff5d42cf8
Alethor <erickrg149@gmail.com>
2019-11-02 14:09:39 -0300

    Limpando imports inúteis

--- ufpr_application/src/main/java/br/ufprconvida/security/WebSecurityConfig.java
+++ ufpr_application/src/main/java/br/ufprconvida/security/WebSecurityConfig.java
@@ -12,14 +12,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.ufprconvida.config.JwtAuthenticationEntryPoint;
 import br.ufprconvida.config.JwtRequestFilter;
-import br.ufprconvida.services.JwtUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -29,9 +27,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Autowired
-    private UserDetailsService jwtUserDetailsService;
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
